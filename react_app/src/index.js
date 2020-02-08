2f9a232e11c4165bb0244a32827cbcc4153224d9
Chathumini Abeyratna <chathuminiabeyratna@Chathuminis-MacBook-Air-2.local>
2020-02-08 18:27:25 -0500

    commit

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import './css/menu.scss';
+import './css/viewSpending.scss';
 import NavBar from './components/NavBar.js';
 import Header from './components/Header.js';
+import ViewSpending from './components/ViewSpending.js';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <div>
     <Header/>
     <NavBar/>
+    <ViewSpending/>
   </div>,
   document.getElementById('root'));
   serviceWorker.unregister();
